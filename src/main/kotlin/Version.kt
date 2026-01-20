aec2f82c56c3bdea61491294bbd16b490c441d67
Richy <mail@richy.sh>
2026-01-20 11:11:28 +0100

    ver: bump v1.4.0

--- src/main/kotlin/Version.kt
+++ src/main/kotlin/Version.kt
@@ -1,9 +1,9 @@
 package me.richy.radioss
 
 object Version {
-    const val VERSION = "1.3.0"
+    const val VERSION = "1.5.0"
     const val NAME = "Radioss"
     
     val FULL_VERSION = "$NAME v$VERSION"
-    val FOOTER_TEXT = "$NAME • v$VERSION • Made with ❤️"
+    val FOOTER_TEXT = "$NAME • v$VERSION"
 } 
